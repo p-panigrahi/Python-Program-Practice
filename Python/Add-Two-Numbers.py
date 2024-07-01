0e559f86f312838e6ec4d852d04d18057677b064
Prabhudutta Panigrahi <58845544+p-panigrahi@users.noreply.github.com>
2024-07-01 20:07:08 +0530

    Soluation:1

---
+++ Python/Add-Two-Numbers.py
@@ -0,0 +1,10 @@
+# Soluation:1 Pre-defiend Variable
+# num1 = 34
+# num2 = 45
+# result = num1 + num2
+# print("The sum of two number is :" , result)
+
+# Soluation:2 Using User Input Variable
+# num1 = int(input("Enter Your First number :"))
+# num2 = float(input("Enter Your Second number :"))
+# print("The sum of two number is : ", num1 + num2)
